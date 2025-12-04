a328bff3abdd788ec9c3033212ddb134f0c59d89
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2025-12-03 22:11:57 -0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -103,7 +103,7 @@ if( -not $SkipBootstrap )
 
     [Uri[]] $binToolUrls = @(
         'https://dist.nuget.org/win-x86-commandline/v6.10.2/nuget.exe',
-        'https://dot.net/v1/dotnet-install.sh',
+        'https://dot.net/v1/dotnet-install.sh'
         #'https://dot.net/v1/dotnet-install.ps1'
     )
 
